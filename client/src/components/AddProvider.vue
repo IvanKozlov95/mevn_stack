f835dc610eb8a6747f220b62c8514bd50877d517
Ivan Kozlov <ikozlov9000@gmail.com>
2018-05-15 14:39:29 -0700

    Moved adding provider logic to a separate component

---
+++ client/src/components/AddProvider.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <modal name="addprovider-modal" @before-close="onClose">
+      <h1> asdasdasd </h1>
+      <input type="text" name="name" placeholder="Name" v-model="name">
+      <button class="btn" @click="addProvider">Add provider</button>
+    </modal>
+  </div>
+</template>
+
+<script>
+import ProvidersService from '@/services/ProvidersService'
+
+export default {
+  name: 'addprovider',
+
+  props: {
+    onClose: Function
+  },
+
+  data () {
+    return {
+      name: ''
+    }
+  },
+
+  methods: {
+    async addProvider () {
+      await ProvidersService.addProvider({ name: this.name })
+    },
+  }
+}
+
+</script>
