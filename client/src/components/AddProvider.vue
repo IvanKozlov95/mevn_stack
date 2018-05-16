61626d7675a94494167de4641dca52852a76b1fe
Ivan Kozlov <ikozlov9000@gmail.com>
2018-05-16 02:15:35 -0700

    To last

--- client/src/components/AddProvider.vue
+++ client/src/components/AddProvider.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
-    <modal name="addprovider-modal" @before-close="beforeClose">
+    <b-form class="form">
+      <b-form-group>
+        <b-input-group>
+            <b-form-input type="text"
+                          name="provider-name"
+                          placeholder="Provider name"
+                          required
+                          v-model="name"></b-form-input>
+            <b-input-group-append>
+              <b-button variant="primary" @click="addProvider">Add provider</b-button>
+            </b-input-group-append>
+          </b-input-group>
+      </b-form-group>
+    </b-form>
+    <!-- <modal name="addprovider-modal" @before-close="beforeClose">
       <h1> asdasdasd </h1>
       <input type="text" name="name" placeholder="Name" v-model="name">
       <button class="btn" @click="addProvider">Add provider</button>
-    </modal>
+    </modal> -->
   </div>
 </template>
 
@@ -22,17 +36,21 @@ export default {
 
   methods: {
     async addProvider () {
-      await ProvidersService.addProvider({ name: this.name })
-    },
-
-    open () {
-      this.$modal.show('addprovider-modal')
-    },
-
-    beforeClose () {
-      this.$root.$emit('addprovider-modal-close');
+      ProvidersService.addProvider({ name: this.name })
+        .then(() => {
+          this.$root.$emit('providersListChanged')
+          this.$swal(
+            'Done',
+            `New provider has been added!`,
+            'success'
+          )
+        })
+      this.name = ''
     }
   }
 }
 
 </script>
+
+<style scoped>
+</style>
