d4b46a0279145becc1db37d18461501f98fe2a11
Ivan Kozlov <ikozlov9000@gmail.com>
2018-05-16 02:57:24 -0700

    Added providers list component

---
+++ client/src/components/ProvidersList.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <b-table striped
+            bordered
+            :items="providers"
+            :fields="providersDisplayFileds">
+      <template slot="name" slot-scope="data">
+        <b-form-checkbox v-model="providers[data.index].selected"
+                        value="true"
+                        unchecked-value=false>{{ data.item.name }}</b-form-checkbox>
+      </template>
+      <template slot="actions" slot-scope="data">
+        <b-btn size="sm" @click="editProvider(data.item)">Edit</b-btn>
+        <b-btn size="sm" @click="deleteProvider(data.item._id)">Delete</b-btn>
+      </template>
+    </b-table>
+    <modal name="edit-modal">
+      <b-container class="container">
+        <h1> Edit provider </h1>
+        <input type="text" name="name" placeholder="Name" v-model="current.name">
+        <b-button size="sm" variant="primary" @click="updateProvider">Update provider</b-button>
+      </b-container>
+    </modal>
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import ProvidersService from '@/services/ProvidersService'
+
+export default {
+  name: 'providerslist',
+
+  data () {
+    return {
+      providers: [],
+      providersDisplayFileds: [ 'name', 'actions' ],
+      current: {}
+    }
+  },
+
+  mounted () {
+    this.getProviders()
+    this.$root.$on('providersListChanged', () => this.getProviders())
+  },
+
+  methods: {
+    async getProviders () {
+      const response = await ProvidersService.fetchProviders()
+      // todo: save selected on update
+      this.providers = response.data.providers
+    },
+
+    updateProvider () {
+      console.log('update provider')
+    },
+
+    editProvider (current) {
+      this.current = {...current}
+      this.$modal.show('edit-modal')
+    },
+
+    async deleteProvider (id) {
+      if (typeof id !== 'undefined') {
+        await ProvidersService.deleteProvider({ id: id })
+        this.getProviders()
+        // this.$root.$emit('providersListChanged')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -4,16 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VModal from 'vue-js-modal'
+import BootstrapVue from 'bootstrap-vue'
 import VueMultiselect from 'vue-multiselect'
 import VueSweetalert2 from 'vue-sweetalert2'
 import AddProvider from '@/components/AddProvider'
-import BootstrapVue from 'bootstrap-vue'
+import ProvidersList from '@/components/ProvidersList'
 
 Vue.use(VModal)
 Vue.use(BootstrapVue)
 Vue.use(VueSweetalert2)
 Vue.component('addprovider', AddProvider)
 Vue.component('multiselect', VueMultiselect)
+Vue.component('providerslist', ProvidersList)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
