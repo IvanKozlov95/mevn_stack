0f41c0ff5b78391104e3cc190dee8db3398da90b
Ivan Kozlov <ikozlov9000@gmail.com>
2018-05-20 21:53:18 -0700

    Dockerized

--- server/lib/mongoose.js
+++ server/lib/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose		= require('mongoose');
+let mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/'
+mongoUri += 'demo'
 
-mongoose.connect('mongodb://localhost:27017/demo')
+mongoose.connect(mongoUri)
   .then(() => {
-    console.log('Connected to mongoose. Uri: mongodb://localhost:27017/demo');
+    console.log(`Connected to mongoose. Uri: #{mongoUri}`);
   })
   .catch(console.error);
 
